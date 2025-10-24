e14bd75dc8880ff9cd535741b2e11e7f27188a98
woo <khw19980926@gmail.com>
2025-10-24 12:56:12 +0900

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Banner, Box, OverView, Page, SliderBox, SliderWrapper, Title } from './
 import { formatImgUrl } from '../../utils/utils';
 import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 interface Props{
   data:IMovie | undefined;
@@ -11,13 +12,20 @@ interface Props{
 const Home = ({data}:Props) => {
   const [index, setIndex] = useState(0);
   const increase = () => setIndex((prev)=>prev+1);
-  console.log(window.innerWidth)
+  const items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,17,19,20,1,1,1,1,]
+  const [slider,setSlider] = useState(1);
+  const onClick = ()=>{
+      setSlider((prev)=> prev < items.length ? -(2 * window.innerWidth)+38 : prev);
+      console.log('slider',slider)
+  }
+  console.log(items.length)
+
   return (
-    <Page onClick={increase}>
+    <Page onClick={onClick}>
       <Banner img={formatImgUrl(data?.results[0].backdrop_path || "")}>
         <Title>{data?.results[0].title}</Title>
         <OverView>{data?.results[0].overview}</OverView>
-        <SliderWrapper>
+        {/* <SliderWrapper>
           <AnimatePresence>
             <SliderBox
               transition={{ type: "tween" }}
@@ -26,15 +34,50 @@ const Home = ({data}:Props) => {
               exit={{ x: -window.outerWidth  }}
               key={index}
             >
-              {[1, 2, 3, 4, 5, 6].map((item, idx) => (
+              {[1, 2, 3, 4, 5, 6,7,8,9].map((item, idx) => (
                 <Box key={idx}>{item}</Box>
               ))}
             </SliderBox>
           </AnimatePresence>
-        </SliderWrapper>
+        </SliderWrapper> */}
+        <SlideWrapper>
+          <SlideBox x={String(slider)}>
+            {items.map((item,idx)=>(
+              <Card key={idx}>{item}</Card>
+            ))}
+          </SlideBox>
+        </SlideWrapper>
       </Banner>
     </Page>
   );
 }
 
-export default Home
+export default Home;
+
+const SlideWrapper = styled.div`
+  width: 100%;
+  border:1px solid blue;
+  overflow: hidden;
+`;
+
+const SlideBox = styled.div<{x:string}>`
+    display: flex;
+    border:2px solid red;
+    column-gap: 10px;
+    padding:10px 0px;
+    transform: translateX(${(props)=>`${props.x}px`});
+    transition:all .2s ease-in-out;
+`;
+
+const Card = styled.div`
+  flex: 0 0 calc((100% - 10px * 5) / 6); /* 6개 보이도록 너비 계산 */
+  height: 250px;
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  color:black;
+`
